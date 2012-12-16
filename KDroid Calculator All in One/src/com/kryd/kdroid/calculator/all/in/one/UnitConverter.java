6136ed67cf6b365acdca3f81e80bacbbb62989e4
Kryd <Kryd@Kryd-PC>
2012-12-16 05:48:31 +0200

    db

---
+++ KDroid Calculator All in One/src/com/kryd/kdroid/calculator/all/in/one/UnitConverter.java
@@ -0,0 +1,13 @@
+package com.kryd.kdroid.calculator.all.in.one;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UnitConverter extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.unit_converter);
+	}
+}
